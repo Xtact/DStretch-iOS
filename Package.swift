5335e6c7712b174b60300df0a8be49eb3502196f
El Guapo <colbyfitch@yahoo.com>
2025-06-29 19:24:51 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,26 +1,23 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "Dstretch-iOS",
-    platforms: [
-        .iOS(.v14)
-    ],
+    name: "Dstretch",
+    platforms: [.iOS(.v13)],
     products: [
-        .library(name: "Dstretch", targets: ["Dstretch"])
+        .library(
+            name: "Dstretch",
+            targets: ["Dstretch"]
+        ),
     ],
     targets: [
         .target(
             name: "Dstretch",
-            path: ".",
-            exclude: ["README.md", "codemagic.yaml", ".github"],
-            sources: ["."],
-            publicHeadersPath: ""
+            dependencies: []
         ),
         .testTarget(
             name: "DstretchTests",
-            dependencies: ["Dstretch"],
-            path: "Tests"
-        )
+            dependencies: ["Dstretch"]
+        ),
     ]
 )
